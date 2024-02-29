e9b72dfb0fcc79c7081b825aff7dc6b527f383dd
malakggh <malakggh@gmail.com>
2024-02-29 15:50:50 +0200

    Add DataTableColumnHeader and DataTablePagination components

--- academilink/components/ManagerTutorRequests/ManagerTutorRequests.tsx
+++ academilink/components/ManagerTutorRequests/ManagerTutorRequests.tsx
@@ -6,6 +6,21 @@ import { useQuery } from "@tanstack/react-query";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "./data-table";
 
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+
+import { TriangleDownIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DataTableColumnHeader } from "../ui/table/DataTableColumnHeader";
+
 export default function ManagerTutorRequests() {
   const {
     data: tutorRequests,
@@ -21,28 +36,114 @@ export default function ManagerTutorRequests() {
 
   const columns: ColumnDef<tutorRequestsType>[] = [
     {
+      accessorKey: "date",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="תאריך" />
+      ),
+      cell: ({ row }) => {
+        const date = new Date(row.getValue("date"));
+        return (
+          <div className="text-right font-medium">
+            {date.toLocaleDateString("he-IL")}
+          </div>
+        );
+      },
+    },
+
+    {
       accessorKey: "tutor.user.name",
-      header: "Tutor",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="שם מתגבר" />
+      ),
     },
     {
       accessorKey: "courseName",
-      header: "Course Name",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="שם הקורס" />
+      ),
     },
     {
       accessorKey: "courseDepartment",
-      header: "Department",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="מחלקה" />
+      ),
     },
     {
       accessorKey: "courseGrade",
-      header: "Grade",
+      header: () => <div className="text-right">{"ציון"}</div>,
+      cell: ({ row }) => {
+        const courseGrade = parseFloat(row.getValue("courseGrade")) / 100;
+        const formatted = new Intl.NumberFormat("en-US", {
+          style: "percent",
+        }).format(courseGrade);
+
+        return <div className="text-right font-medium">{formatted}</div>;
+      },
     },
     {
       accessorKey: "courseRequestMessage",
-      header: "Message",
+      // header: "Message",
+      header: () => <div className="text-right">{"הודעה"}</div>,
+      cell: ({ row }) => {
+        const request = row.original;
+        const message = request.courseRequestMessage;
+        // handle long text by showing the first 50 char and using dropdown menu to show full text
+        return (
+          <div className="text-right">
+            {message && message.length > 50 ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="flex items-center space-x-2">
+                    <span>{message.slice(0, 50)}...</span>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">{'"הצג עוד"'}</span>
+                      <TriangleDownIcon className="h-4 w-4" />
+                    </Button>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="text-pretty w-[33%]">
+                  <div style={{ direction: "rtl" }}>
+                    <DropdownMenuLabel>{"הודעה"}</DropdownMenuLabel>
+                    <DropdownMenuItem>{message}</DropdownMenuItem>
+                  </div>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              message
+            )}
+          </div>
+        );
+      },
     },
     {
-      accessorKey: "date",
-      header: "Date",
+      id: "actions",
+      cell: ({ row }) => {
+        const request = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() =>
+                  navigator.clipboard.writeText(request.courseName)
+                }
+              >
+                Copy course name
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
     },
   ];
 
--- academilink/components/ManagerTutorRequests/data-table.tsx
+++ academilink/components/ManagerTutorRequests/data-table.tsx
@@ -4,7 +4,12 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
+  PaginationState,
+  SortingState,
+  getSortedRowModel,
+  VisibilityState,
 } from "@tanstack/react-table";
 
 import {
@@ -16,62 +21,132 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-interface DataTableProps<TData> {
-  columns: ColumnDef<TData>[];
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { DataTablePagination } from "../ui/table/DataTablePagination";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
 
-export function DataTable<TData>({ columns, data }: DataTableProps<TData>) {
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 5,
+  });
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
   const table = useReactTable({
     data,
     columns,
+    // debugTable: true,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      pagination,
+      sorting,
+      columnVisibility,
+    },
   });
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
+    <div>
+      <div className="flex items-center py-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              {"הצג עמודות"}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
                 return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
                 );
               })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
     </div>
   );
 }
