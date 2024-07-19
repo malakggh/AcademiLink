361b66043ebe5d0f23ecca882cac72a545178866
malakggh <malakggh@gmail.com>
2024-07-19 17:21:28 +0300

    chore: Update ManagerStats component to load images

--- academilink/components/ManagerStats/ManagerStats.tsx
+++ academilink/components/ManagerStats/ManagerStats.tsx
@@ -2,48 +2,36 @@
 
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Image from "next/image";
 export default function ManagerStats() {
   const { data, error, isLoading } = useQuery({
     queryKey: ["session-completion-rates"],
     queryFn: async () => {
-      const { data } = await axios(
-        {
-          url: "http://127.0.0.1:8000/api/stats/session-completion-rates",
-          method: "get",
-        }
-        // "http://127.0.0.1:8000/api/stats/session-completion-rates",
-        // {
-        //   withCredentials: false,
-        // },
-      );
-      console.log(data);
-      return data;
+      const { data } = await axios({
+        url: "http://127.0.0.1:8000/api/stats/session-completion-rates",
+        method: "get",
+        responseType: "blob",
+      });
+      const imageObjectUrl = URL.createObjectURL(data);
+      if (!imageObjectUrl) {
+        throw new Error("Failed to create object URL");
+      }
+      return imageObjectUrl;
     },
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>An error occurred: {error.message}</div>;
-
   return (
-    <div>
-      <h1>Session Completion Rates</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <>
+      {!isLoading && !error && data && (
+        <>
+          <Image
+            src={data}
+            width={750}
+            height={750}
+            alt="Session Completion Rates"
+          />
+        </>
+      )}
+    </>
   );
 }
-
-//   if (isLoading) return <div>Loading...</div>;
-//   if (error) return <div>Error: {error.message}</div>;
-
-//   // Display the data based on its type
-//   const content = data?.imageUrl ? (
-//     <img src={data.imageUrl} alt="Session Completion Rates" />
-//   ) : (
-//     <pre>{JSON.stringify(data, null, 2)}</pre>
-//   );
-
-//   return (
-//     <div>
-//       {content}
-//     </div>
-//   );
--- academilink/fast_api/app/main.py
+++ academilink/fast_api/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Set up CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000/"],  # Allows all origins from localhost:3000
+    allow_origins=["http://localhost:3000"],  # Allows all origins from localhost:3000
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
