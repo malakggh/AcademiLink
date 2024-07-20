eb3940759f36039fca942aa9e18cc91dfa78b380
haya166 <hayahamed07@gmail.com>
2024-07-20 17:47:47 +0300

    magager stats feature

--- academilink/components/ManagerStats/ManagerStats.tsx
+++ academilink/components/ManagerStats/ManagerStats.tsx
@@ -1,36 +1,137 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
+import JSZip from "jszip";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { toast } from "../ui/use-toast";
+import { links } from "./StatsLinks";
+
 export default function ManagerStats() {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["session-completion-rates"],
-    queryFn: async () => {
-      const { data } = await axios({
-        url: "http://127.0.0.1:8000/api/stats/session-completion-rates",
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
+  const [images, setImages] = useState<string[]>([]);
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: async ({
+      stat,
+    }: {
+      stat:
+        | "session-completion-rates"
+        | "tutor-course-distribution"
+        | "monthly-session-trends"
+        | "tutor-sessions"
+        | "student-hours";
+    }) => {
+      const { data, headers } = await axios({
+        url: links[stat],
         method: "get",
         responseType: "blob",
       });
-      const imageObjectUrl = URL.createObjectURL(data);
-      if (!imageObjectUrl) {
-        throw new Error("Failed to create object URL");
+      return { data, headers };
+    },
+    onError: (error) => {
+      toast({
+        variant: "destructive",
+        title: "Error handling request",
+        description: error.message,
+      });
+    },
+    onSuccess: async ({ data, headers }) => {
+      setSelectedImageIndex(0);
+      setImages([]);
+      if (headers["content-type"] === "image/png") {
+        const imageObjectUrl = URL.createObjectURL(data);
+        if (!imageObjectUrl) {
+          throw new Error("Failed to create object URL");
+        }
+        setImages([imageObjectUrl]);
+      } else if (headers["content-type"] === "application/zip") {
+        const zip = await JSZip.loadAsync(data);
+        const imageFiles = await Promise.all(
+          Object.keys(zip.files)
+            .filter((file) => file.endsWith(".png"))
+            .map(async (file) => {
+              const content = await zip.files[file].async("blob");
+              return URL.createObjectURL(content);
+            })
+        );
+        setImages(imageFiles);
       }
-      return imageObjectUrl;
+      toast({
+        title: "Request has been handled successfully",
+        description: "Data fetched successfully",
+      });
     },
   });
-
   return (
     <>
-      {!isLoading && !error && data && (
-        <>
-          <Image
-            src={data}
-            width={750}
-            height={750}
-            alt="Session Completion Rates"
-          />
-        </>
+      <Button
+        onClick={() => {
+          mutate({ stat: "session-completion-rates" });
+        }}
+        variant="outline"
+        className="ml-auto"
+      >
+        {"התפלגות לקיום התגבורים"}
+      </Button>
+      <Button
+        onClick={() => {
+          mutate({ stat: "tutor-course-distribution" });
+        }}
+        variant="outline"
+        className="ml-auto"
+      >
+        {"tutor-course-distribution"}
+      </Button>
+      <Button
+        onClick={() => {
+          mutate({ stat: "monthly-session-trends" });
+        }}
+        variant="outline"
+        className="ml-auto"
+      >
+        {"monthly-session-trends"}
+      </Button>
+      <Button
+        onClick={() => {
+          mutate({ stat: "tutor-sessions" });
+        }}
+        variant="outline"
+        className="ml-auto"
+      >
+        {"tutor-sessions"}
+      </Button>
+      <Button
+        onClick={() => {
+          mutate({ stat: "student-hours" });
+        }}
+        variant="outline"
+        className="ml-auto"
+      >
+        {"student-hours"}
+      </Button>
+      {!isPending && !isError && images.length > 0 && (
+        <Image
+          src={images[selectedImageIndex]}
+          width={750}
+          height={750}
+          alt="Selected Image"
+        />
+      )}
+      {!isPending && !isError && images.length > 0 && (
+        <div>
+          {images.map((img, idx) => (
+            <Button
+              key={idx}
+              onClick={() => setSelectedImageIndex(idx)}
+              variant="outline"
+              className="ml-auto"
+            >
+              {`Image ${idx + 1}`}
+            </Button>
+          ))}
+        </div>
       )}
     </>
   );
---
+++ academilink/components/ManagerStats/StatsLinks.ts
@@ -0,0 +1,10 @@
+export const links = {
+  "session-completion-rates":
+    "http://127.0.0.1:8000/api/stats/session-completion-rates",
+  "tutor-course-distribution":
+    "http://127.0.0.1:8000/api/stats/tutor-course-distribution",
+  "monthly-session-trends":
+    "http://127.0.0.1:8000/api/stats/monthly-session-trends?test=true",
+  "tutor-sessions": "http://127.0.0.1:8000/api/stats/tutor-sessions",
+  "student-hours": "http://127.0.0.1:8000/api/stats/student-hours",
+};
